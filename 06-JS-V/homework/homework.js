4bd352664752e5eb6ec50775fd915f152f09e3d2
Santirbe98 <santirbe@gmail.com>
2022-05-19 19:33:22 -0300

    Ejercicio V terminado

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -59,21 +59,30 @@ function agregarStringInvertida() {
     //  }
 
   class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
+    constructor(nombre, apellido, edad, domicilio) {
       // Crea el constructor:
-
-    }
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+    }    
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
+  let datos = new Persona ("Juan", "Perez", 22, "Saavedra 123");
+  return datos;
 }
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function (){
+    return this.nombre + ", " + this.edad + " años";
+    
+  }
 }
   
 
